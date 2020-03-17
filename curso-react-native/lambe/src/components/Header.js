b963dcb1a49e7043364c79c7a0112c1b89610a41
Marcos Aurelio <marcos.aucorrea@gmail.com>
2020-03-17 10:13:36 -0300

    Adicionado usuario no header.

--- curso-react-native/lambe/src/components/Header.js
+++ curso-react-native/lambe/src/components/Header.js
@@ -1,14 +1,27 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {Gravatar} from 'react-native-gravatar';
 import {StyleSheet, Text, View, Image} from 'react-native';
 import icon from '../../assets/imgs/icon.png';
 
 class Header extends Component {
   render() {
+    const name = this.props.name || 'Anonymous';
+    const gravatar = this.props.email ? (
+      <Gravatar
+        options={{email: this.props.email, secure: true}}
+        style={styles.avatar}
+      />
+    ) : null;
     return (
       <View style={styles.container}>
         <View style={styles.rowContainer}>
           <Image source={icon} style={styles.image} />
           <Text style={styles.title}>Lambe Lambe</Text>
+        </View>
+        <View style={styles.userContainer}>
+          <Text style={styles.user}>{name}</Text>
+          {gravatar}
         </View>
       </View>
     );
@@ -21,6 +34,8 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderColor: '#BBB',
     width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   rowContainer: {
     flexDirection: 'row',
@@ -37,6 +52,26 @@ const styles = StyleSheet.create({
     height: 30,
     fontSize: 20,
   },
+  userContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  user: {
+    fontSize: 10,
+    color: '#888',
+  },
+  avatar: {
+    width: 30,
+    height: 30,
+    marginLeft: 10,
+  },
 });
 
-export default Header;
+const mapStateToProps = ({user}) => {
+  return {
+    email: user.email,
+    name: user.name,
+  };
+};
+
+export default connect(mapStateToProps)(Header);
